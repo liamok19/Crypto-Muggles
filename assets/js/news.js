7818ef2b708ce72b6e1732df60c0ec751d6befd0
Liam Ok <liamokane19@gmail.com>
2022-01-17 21:14:15 +1100

    applying button click to each row in the news container

--- assets/js/news.js
+++ assets/js/news.js
@@ -28,67 +28,89 @@ var  APIKey="apikey=pub_3691ecb37bd967f43f3029e801b78e631f93";
 
 //https://newsdata.io/api/1/news?apikey=pub_3607c98c284f7ae81a7c5cb1e10e5d4eb329&q=cryptocurrency
 //fetch from newsdata API, attaching the API var and the parameter for cryptocurrency
-// fetch('https://newsdata.io/api/1/news?' + APIKey + '&q=cryptocurrency&country=us,au,ca&language=en' )
-//     // fetch('https://cryptopanic.com/api//v1/posts/?auth_token=65359dbd6e66041b361c14cef4b02d200ad629cd&public=true')
-//     .then(response => response.json())
-//     // .then(data => console.log (data))
-//     .then(data => {
-//         var timeValue = data ['results'][0]['pubDate'];
-//         var sourceValue = data ['results'][0]['source_id'];
-//         var titleValue = data ['results'][0]['title'];
+fetch('https://newsdata.io/api/1/news?' + APIKey + '&q=cryptocurrency&country=us,au,ca&language=en' )
+    // fetch('https://cryptopanic.com/api//v1/posts/?auth_token=65359dbd6e66041b361c14cef4b02d200ad629cd&public=true')
+    .then(response => response.json())
+    // .then(data => console.log (data))
+    .then(data => {
+        var timeValue = data ['results'][0]['pubDate'];
+        var sourceValue = data ['results'][0]['source_id'];
+        var titleValue = data ['results'][0]['title'];
 
-//         timeEl.innerHTML=timeValue;
-//         creatorValue.innerHTML= sourceValue;
-//         articleTitleEl.innerHTML= titleValue;
+        timeEl.innerHTML=timeValue;
+        creatorValue.innerHTML= sourceValue;
+        articleTitleEl.innerHTML= titleValue;
 
-//         var timeValue2 = data ['results'][1]['pubDate'];
-//         var sourceValue2 = data ['results'][1]['source_id'];
-//         var titleValue2 = data ['results'][1]['title'];
+        var timeValue2 = data ['results'][1]['pubDate'];
+        var sourceValue2 = data ['results'][1]['source_id'];
+        var titleValue2 = data ['results'][1]['title'];
 
-//         timeEl2.innerHTML=timeValue2;
-//         creatorValue2.innerHTML= sourceValue2;
-//         articleTitleEl2.innerHTML= titleValue2;
+        timeEl2.innerHTML=timeValue2;
+        creatorValue2.innerHTML= sourceValue2;
+        articleTitleEl2.innerHTML= titleValue2;
 
-//         var timeValue3 = data ['results'][2]['pubDate'];
-//         var sourceValue3 = data ['results'][2]['source_id'];
-//         var titleValue3 = data ['results'][2]['title'];
+        var timeValue3 = data ['results'][2]['pubDate'];
+        var sourceValue3 = data ['results'][2]['source_id'];
+        var titleValue3 = data ['results'][2]['title'];
 
-//         timeEl3.innerHTML= timeValue3;
-//         creatorValue3.innerHTML= sourceValue3;
-//         articleTitleEl3.innerHTML= titleValue3;
+        timeEl3.innerHTML= timeValue3;
+        creatorValue3.innerHTML= sourceValue3;
+        articleTitleEl3.innerHTML= titleValue3;
 
-//         var timeValue4 = data ['results'][3]['pubDate'];
-//         var sourceValue4 = data ['results'][3]['source_id'];
-//         var titleValue4 = data ['results'][3]['title'];
+        var timeValue4 = data ['results'][3]['pubDate'];
+        var sourceValue4 = data ['results'][3]['source_id'];
+        var titleValue4 = data ['results'][3]['title'];
 
-//         timeEl4.innerHTML= timeValue4;
-//         creatorValue4.innerHTML= sourceValue4;
-//         articleTitleEl4.innerHTML= titleValue4;
+        timeEl4.innerHTML= timeValue4;
+        creatorValue4.innerHTML= sourceValue4;
+        articleTitleEl4.innerHTML= titleValue4;
 
-//         var timeValue5 = data ['results'][4]['pubDate'];
-//         var sourceValue5 = data ['results'][4]['source_id'];
-//         var titleValue5 = data ['results'][4]['title'];
+        var timeValue5 = data ['results'][4]['pubDate'];
+        var sourceValue5 = data ['results'][4]['source_id'];
+        var titleValue5 = data ['results'][4]['title'];
 
-//         timeEl5.innerHTML= timeValue5;
-//         creatorValue5.innerHTML= sourceValue5;
-//         articleTitleEl5.innerHTML= titleValue5;
+        timeEl5.innerHTML= timeValue5;
+        creatorValue5.innerHTML= sourceValue5;
+        articleTitleEl5.innerHTML= titleValue5;
 
-// })
 
-var btn0 = document.getElementById("articleNewsRow0");
-var btn1 = document.getElementById("articleNewsRow1");
-var btn2 = document.getElementById("articleNewsRow2");
-var btn3 = document.getElementById("articleNewsRow3");
-var btn4 = document.getElementById("articleNewsRow4");
 
-console.log(btn0 + 0);
-console.log(btn1+ 1);
-console.log(btn2+ 2);
-console.log(btn3+ 3);
-console.log(btn4+ 4);
+    var btn0 = document.getElementById("articleNewsRow0");
+    var btn1 = document.getElementById("articleNewsRow1");
+    var btn2 = document.getElementById("articleNewsRow2");
+    var btn3 = document.getElementById("articleNewsRow3");
+    var btn4 = document.getElementById("articleNewsRow4");
 
-btn0.addEventListener("click", function (event) {
-    event.preventDefault();
-    this.textContent = "hello"
+    console.log(btn0 + 0);
+    console.log(btn1+ 1);
+    console.log(btn2+ 2);
+    console.log(btn3+ 3);
+    console.log(btn4+ 4);
 
+    btn0.addEventListener("click", function (event) {
+        event.preventDefault();
+        this.textContent = "hello"
+
+    })
+
+    btn1.addEventListener("click", function (event) {
+        event.preventDefault();
+        this.textContent = "there"
+
+    })
+    btn2.addEventListener("click", function (event) {
+        event.preventDefault();
+        this.textContent = "you're"
+
+    })
+    btn3.addEventListener("click", function (event) {
+        event.preventDefault();
+        this.textContent = "in"
+
+    })
+    btn4.addEventListener("click", function (event) {
+        event.preventDefault();
+        this.textContent = "Crypto"
+
+    })
 })
