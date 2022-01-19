f73fafaf8a7f033b0cff61d56b10b71e37822dfa
Tirta Styadi <tirta.setiady@yahoo.com>
2022-01-19 19:42:27 +1100

    MOD: Modify web become responsive web

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,3 @@ var timeAEDT = document.querySelector(".currentTime");
 
 timeAEDT.innerHTML = moment().format("h:mm:ss A ") + " AEDT";
 // // moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); // "Sunday, February 14th 2010, 3:25:50 pm"
-
